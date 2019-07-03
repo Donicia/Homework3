735f7ed9c871b99c4d35fcb277720f8427b337be
Donicia <51253482+Donicia@users.noreply.github.com>
2019-07-02 20:37:55 -0500

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -33,20 +33,34 @@ print("----------------------------")
 print(f"Total Months: {len(date)}")
 print(f"Total Revenue: ${sum(Profit_Losses)}")
 print(f"Average Revenue Change: ${sum(Profit_Losses_Change)/len(Profit_Losses_Change)}")
-print(f"Greatest Increase in Revenue: {date[Profit_Losses_Change.index(max(Profit_Losses_Change))+1]} ({max(Profit_Losses_Change)})")
-print(f"Greatest Decrease in Revenue: {date[Profit_Losses_Change.index(min(Profit_Losses_Change))+1]} ({min(Profit_Losses_Change)})\n")
+print(f"Greatest Increase in Revenue: {date[Profit_Losses_Change.index(max(Profit_Losses_Change))+1]} (${max(Profit_Losses_Change)})")
+print(f"Greatest Decrease in Revenue: {date[Profit_Losses_Change.index(min(Profit_Losses_Change))+1]} (${min(Profit_Losses_Change)})\n")
 
 
-Output_file = r'C:\Users\donic\Desktop\Homework\Homework 3\Python Homework\PyBank\PyBank.txt'
+PyBank = r'C:\Users\donic\Desktop\Homework\Homework 3\Python Homework\PyBank\PyBank.txt'
 
-with open(Output_file) as txt_file:
-    txt_file.write(f"Total Months: {len(date)}")
-    txt_file.write("\n")
-    txt_file.write(f"Total Revenue: ${sum(Profit_Losses)}")
-    txt_file.write("\n")
-    txt_file.write(f"Average Revenue Change: ${sum(Profit_Losses_Change)/len(Profit_Losses_Change)}")
-    txt_file.write("\n")
-    txt_file.write(f"Greatest Increase in Revenue: {date[Profit_Losses_Change.index(max(Profit_Losses_Change))+1]} ({max(Profit_Losses_Change)})")
-    txt_file.write("\n")
-    txt_file.write(f"Greatest Decrease in Revenue: {date[Profit_Losses_Change.index(min(Profit_Losses_Change))+1]} ({min(Profit_Losses_Change)})\n")
 
+lines = ("Financial Analysis")
+lines_22 = ("----------------------------")
+line_11 = (f"Total Months: {len(date)}")
+lines_1 = (f"Total Revenue: ${sum(Profit_Losses)}")
+lines_2 = (f"Average Revenue Change: ${sum(Profit_Losses_Change)/len(Profit_Losses_Change)}")
+lines_4 = (f"Greatest Increase in Revenue: {date[Profit_Losses_Change.index(max(Profit_Losses_Change))+1]} (${max(Profit_Losses_Change)})")
+lines_5 = (f"Greatest Decrease in Revenue: {date[Profit_Losses_Change.index(min(Profit_Losses_Change))+1]} (${min(Profit_Losses_Change)})\n")
+
+
+
+with open("PyBank.txt", "w") as output:
+    output.write(str(lines))
+    output.write('\n')
+    output.write(str(line_11))
+    output.write('\n')
+    output.write(str(lines_22))
+    output.write('\n')
+    output.write(str(lines_1))
+    output.write('\n')
+    output.write(str(lines_2))
+    output.write('\n')
+    output.write(str(lines_4))
+    output.write('\n')
+    output.write(str(lines_5))
